ec80ede536fea87cf4abf0f31ada39e83ae33b07
Saad Bin Khalid <saadbkhalid666@gmail.com>
2026-04-14 23:33:32 -0700

    setup completion

--- backend/index.js
+++ backend/index.js
@@ -4,6 +4,8 @@ const dotenv = require('dotenv')
 dotenv.config()
 
 
+ 
+
 app.listen(process.env.PORT,()=>{
     console.log(`Server is running on port ${process.env.PORT}`)
 })
--- frontend/App.js
+++ frontend/App.js
@@ -1,6 +1,9 @@
- 
-export default function App() {
- 
-}
+import { Text, View } from "react-native";
 
- 
+export default function App() {
+  return (
+    <View className="text-green-400 ">
+      <Text >Saad Bin Khalid | Full Stack Developer</Text>
+    </View>
+  );
+}
