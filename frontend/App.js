4e1ee63ef4c960c1fa1acad499b21b9d89a55edf
Saad Bin Khalid <saadbkhalid666@gmail.com>
2026-04-15 22:17:17 -0700

    Home.js

--- frontend/App.js
+++ frontend/App.js
@@ -1,9 +1,10 @@
-import { Text, View } from "react-native";
+import { View } from "react-native";
+import { Home } from "./pages/Home";
 
 export default function App() {
   return (
-    <View className="text-green-400 ">
-      <Text >Saad Bin Khalid | Full Stack Developer</Text>
+    <View>
+      <Home />
     </View>
   );
 }
---
+++ frontend/pages/Home.js
@@ -0,0 +1,40 @@
+import { Button, Image, StyleSheet, Text, View } from "react-native";
+
+export const Home = () => {
+  return (
+    <View>
+      <View style={styles.container}>
+        <Image style={styles.image} source={require("../assets/icon.png")} />
+        <Text   style={styles.button}  onPress={()=> console.warn("Button is Pressed!")}>Press To Register</Text>
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+    container:{
+        display:"flex"
+        ,justifyContent:"center",
+        alignItems:"center",
+        height:"100%",
+        width:"100%",
+        backgroundColor:"#000",
+    },
+
+    image: {
+    width: 200,
+    height: 200,
+    resizeMode: "contain",
+
+  },    
+  button:{
+    backgroundColor:"#86e72bff",
+    color:"#fff",
+    fontSize:20,
+    fontWeight:"bold",
+    padding:10,
+    borderRadius:10,
+    
+  }
+  
+}); 
