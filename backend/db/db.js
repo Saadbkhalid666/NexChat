6fe5e6421e93d9d9ca128e17644b67fa230156b2
Saad Bin Khalid <saadbkhalid666@gmail.com>
2026-04-19 01:02:59 -0700

    db.js

---
+++ backend/db/db.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+
+const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log("Mongodb Connected Successfully!");
+  } catch (e) {
+    console.log(e.message);
+  }
+};
+
+module.exports = connectDB
