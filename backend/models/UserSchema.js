596f93fce48f305a3e0b102d83f317ace55f775d
Saad Bin Khalid <saadbkhalid666@gmail.com>
2026-04-19 01:18:44 -0700

    userSchema.js

---
+++ backend/models/UserSchema.js
@@ -0,0 +1,39 @@
+const {Schema,model} = require('mongoose')
+
+const bcrypt = require('bcrypt')
+
+const UserSchema = new Schema({
+    username:{
+        type:String,
+        required:true,
+        unique:true
+    },
+    email:{
+        type:String,
+        required:true,
+        unique:true
+    },
+    password:{
+        type:String,
+        required:true,
+        minLength:8
+    },
+    isOnline:{
+        type:Boolean,
+        default:false
+    }
+    ,createdAt:{
+        type:Date,
+        default:Date.now
+    } 
+
+})
+
+UserSchema.pre('save',async function(next){
+    if(this.isModified('password')){
+        this.password = await bcrypt.hash(this.password,10)
+    }
+    next()
+})
+
+module.exports = model('User',UserSchema)
