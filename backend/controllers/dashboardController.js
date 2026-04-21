2a30ea42a2801e628ec04c058523de73d09212a3
Saad Bin Khalid <saadbkhalid666@gmail.com>
2026-04-20 22:58:29 -0700

    update User

--- backend/controllers/dashboardController.js
+++ backend/controllers/dashboardController.js
@@ -21,6 +21,17 @@ const deleteMessage = async (req,res) => {
     }
 }
 
+const updateUser = async (req,res) => {
+    try{
+        const id = req.params
+        const user = await User.findByIdAndUpdate(id,req.body)
+        res.status(200).json({message:"User updated successfully",user})
+    } catch (error){
+        res.status(500).json({message:"Internal Server Error",error:error.message})
+    }
+}
 
 
-module.exports = {getMessage}
+
+
+module.exports = {getMessage,deleteMessage,updateUser}
