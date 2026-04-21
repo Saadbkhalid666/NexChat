d6daf75a901e3492259f3578c78295a89fccce61
Saad Bin Khalid <saadbkhalid666@gmail.com>
2026-04-20 23:13:37 -0700

    admin routes

--- backend/controllers/dashboardController.js
+++ backend/controllers/dashboardController.js
@@ -1,54 +1,75 @@
-const User = require('../models/UserSchema')
-const Message = require('../models/MessageSchema')
-const getMessage = async (req,res) =>{
-    try {
-        const messages = await Message.find()
-        res.status(200).json({message:"Messages fetched successfully",messages})
+const User = require("../models/UserSchema");
+const Message = require("../models/MessageSchema");
 
-    } catch (error) {
-     res.status(500).json({message:"Internal Server Error",error:error.message})   
-    }
-}
+const getMessage = async (req, res) => {
+  try {
+    const messages = await Message.find();
 
-const deleteMessage = async (req,res) => {
-    try{
+    res.status(200).json({
+      message: "Messages fetched successfully",
+      messages
+    });
 
-        const id = req.params
-        const message = await Message.findByIdAndDelete(id)
-        res.status(200).json({message:"Message deleted successfully",message})
-    } catch (error){
-        res.status(500).json({message:"Internal Server Error",error:error.message})
-    }
-}
+  } catch (error) {
+    res.status(500).json({
+      message: "Internal Server Error",
+      error: error.message
+    });
+  }
+};
 
-const updateUser = async (req,res) => {
-    try{
-        const id = req.params
-        const user = await User.findByIdAndUpdate(id,req.body)
-        res.status(200).json({message:"User updated successfully",user})
-    } catch (error){
-        res.status(500).json({message:"Internal Server Error",error:error.message})
-    }
-}
+const deleteMessage = async (req, res) => {
+  try {
+    const { id } = req.params;
 
-const deleteUser = async (req,res) => {
-    try{
-        const id = req.params
-        const user = await User.findByIdAndDelete(id)
-        res.status(200).json({message:"User deleted successfully",user})
-    } catch (error){
-        res.status(500).json({message:"Internal Server Error",error:error.message})
-    }
-}
+    const message = await Message.findByIdAndDelete(id);
 
-const getUsers = async (req,res) => {
-    try{
+    res.status(200).json({
+      message: "Message deleted successfully",
+      data: message
+    });
 
-        const users = User.find()
-        res.status(200).json({message:"User Fetched Successfully!", usrers})
-    }catch (error){
-        res.status(500).json({message:"Internal Server Error!", error: error.message})
-    }
-}
+  } catch (error) {
+    res.status(500).json({
+      message: "Internal Server Error",
+      error: error.message
+    });
+  }
+};
 
-module.exports = {getMessage,deleteMessage,updateUser,deleteUser,getUsers}
+const deleteUser = async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    const user = await User.findByIdAndDelete(id);
+
+    res.status(200).json({
+      message: "User deleted successfully",
+      data: user
+    });
+
+  } catch (error) {
+    res.status(500).json({
+      message: "Internal Server Error",
+      error: error.message
+    });
+  }
+};
+
+const getUsers = async (req, res) => {
+  try {
+    const users = await User.find();
+
+    res.status(200).json({
+      message: "Users fetched successfully",
+      users
+    });
+
+  } catch (error) {
+    res.status(500).json({
+      message: "Internal Server Error",
+      error: error.message
+    });
+  }
+};
+module.exports = {getMessage,deleteMessage,deleteUser,getUsers}
