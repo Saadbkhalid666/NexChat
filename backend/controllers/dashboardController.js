715fdb807dbffc299a7431e1c42025c6b4308a3e
Saad Bin Khalid <saadbkhalid666@gmail.com>
2026-04-20 22:55:37 -0700

    getMessages

---
+++ backend/controllers/dashboardController.js
@@ -0,0 +1,16 @@
+const User = require('../models/UserSchema')
+const Message = require('../models/MessageSchema')
+const getMessage = async (req,res) =>{
+    try {
+        const messages = await Message.find()
+        res.status(200).json({message:"Messages fetched successfully",messages})
+
+    } catch (error) {
+     res.status(500).json({message:"Internal Server Error",error:error.message})   
+    }
+}
+
+
+
+
+module.exports = {getMessage}
