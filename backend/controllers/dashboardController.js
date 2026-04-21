03426b09c1b57cd98a49e95b30293f846a38e14c
Saad Bin Khalid <saadbkhalid666@gmail.com>
2026-04-20 22:58:48 -0700

    delete User

--- backend/controllers/dashboardController.js
+++ backend/controllers/dashboardController.js
@@ -31,7 +31,15 @@ const updateUser = async (req,res) => {
     }
 }
 
+const deleteUser = async (req,res) => {
+    try{
+        const id = req.params
+        const user = await User.findByIdAndDelete(id)
+        res.status(200).json({message:"User deleted successfully",user})
+    } catch (error){
+        res.status(500).json({message:"Internal Server Error",error:error.message})
+    }
+}
 
 
-
-module.exports = {getMessage,deleteMessage,updateUser}
+module.exports = {getMessage,deleteMessage,updateUser,deleteUser}
