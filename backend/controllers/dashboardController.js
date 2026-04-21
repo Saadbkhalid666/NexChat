9fe082e9b47fbd3352d23b733b3daf61491cd58b
Saad Bin Khalid <saadbkhalid666@gmail.com>
2026-04-20 22:57:26 -0700

    deleteMessage

--- backend/controllers/dashboardController.js
+++ backend/controllers/dashboardController.js
@@ -10,6 +10,16 @@ const getMessage = async (req,res) =>{
     }
 }
 
+const deleteMessage = async (req,res) => {
+    try{
+
+        const id = req.params
+        const message = await Message.findByIdAndDelete(id)
+        res.status(200).json({message:"Message deleted successfully",message})
+    } catch (error){
+        res.status(500).json({message:"Internal Server Error",error:error.message})
+    }
+}
 
 
 
